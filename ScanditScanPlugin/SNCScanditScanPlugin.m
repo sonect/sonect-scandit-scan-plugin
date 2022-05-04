19a306cae75e2c3a69db6b30b45ac4e8169c294f
Ivan Yanakiev <ivan.yanakiev@sonect.ch>
2022-05-04 12:25:38 +0300

    Added auto video resolution to camera settings.

--- ScanditScanPlugin/SNCScanditScanPlugin.m
+++ ScanditScanPlugin/SNCScanditScanPlugin.m
@@ -62,7 +62,7 @@
     }
     
     SDCCameraSettings *cameraSettings = SDCBarcodeCapture.recommendedCameraSettings;
-    
+    cameraSettings.preferredResolution = SDCVideoResolutionAuto;
     self.camera = [SDCCamera cameraAtPosition:cameraPosition];
     [self.camera applySettings:cameraSettings completionHandler:nil];
     [self.context setFrameSource:self.camera completionHandler:nil];
